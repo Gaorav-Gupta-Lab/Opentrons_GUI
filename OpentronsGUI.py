e2271b7c1708e270da2945092b1d14d1b6d49cad
Dennis Simpson <g2.checkpoint@gmail.com>
2020-12-18 16:50:00 -0500

    Error Checking Now Checks Everything

--- OpentronsGUI.py
+++ OpentronsGUI.py
@@ -9,38 +9,41 @@ Chapel Hill, NC  27599-7295
 
 Copyright 2021
 """
-import subprocess
+
+import io
+import shutil
 import sys
 import os
+from TemplateErrorChecking import TemplateErrorChecking
+from opentrons.simulate import simulate, format_runlog
 from UI_MainWindow import Ui_MainWindow
 from PySide2 import QtWidgets, QtGui, QtCore
 from PySide2.QtWidgets import QApplication
-# from PySide2.QtCore import QFile, QObject, QTimer, SIGNAL, QTime
-from paramiko import SSHClient, AutoAddPolicy, RSAKey
+from paramiko import SSHClient, AutoAddPolicy
+from contextlib import redirect_stdout
 from scp import SCPClient
-import Tool_Box
 
-__version__ = "0.1.1"
+__version__ = "0.3.0"
 # pyside2-uic MainWindow.ui -o UI_MainWindow.py
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self, *args, obj=None, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
         self.setupUi(self)
         # Override the window label.
         self.label.setText("Gupta Lab Opentrons GUI v{}".format(__version__))
-
-        self.tsv_file_select_btn.pressed.connect(self.transfer_tsv_file)
+        self.path_to_tsv = None
+        self.selected_program = None
+        self.path_to_program = None
+        self.program_error = True
+        self.temp_tsv_path = "D:{}TempTSV.tsv".format(os.sep)
+        self.tsv_file_select_btn.pressed.connect(self.select_file)
         self.closeGUI_btn.pressed.connect(self.exit_gui)
         self.simulate_run_btn.pressed.connect(self.simulate_run)
-
-        self.select_program_combobx.addItems(["Generic_PCR_v0.1.0", "Illumina_Dual_Indexing"])
-        self.selected_program = None
+        self.select_program_combobx.addItems(["Generic PCR", "Illumina_Dual_Indexing"])
         self.select_program_combobx.currentTextChanged.connect(self.program_name)
-
-        self.path_to_file = None
 
     @staticmethod
     def exit_gui():
@@ -52,94 +55,120 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def tr(self, text, **kwargs):
         return QtCore.QObject.tr(self, text)
 
-    def select_file(self, source):
-        self.path_to_file, _ = \
-            QtWidgets.QFileDialog.getOpenFileName(self, self.tr(source),
+    def select_file(self):
+        self.path_to_tsv, _ = \
+            QtWidgets.QFileDialog.getOpenFileName(self, self.tr("File Select"),
                                                   self.tr("C:{0}Users{0}robotron{0}Documents{0}".format(os.sep)))
 
+        shutil.copyfile(self.path_to_tsv, self.temp_tsv_path)
+
     def transfer_tsv_file(self):
-        if not self.path_to_file:
-            self.select_file("File Transfer Select")
+        if not self.path_to_tsv:
+            self.select_file()
 
-        if self.path_to_file:
-            # ToDo: get robot IP dynamically
-            host_ip = '169.254.254.151'
-            server_path = "/var/lib/jupyter/notebooks/ProcedureFile.tsv"
+        # ToDo: get robot IP dynamically
+        host_ip = '169.254.254.151'
+        server_path = "/var/lib/jupyter/notebooks/ProcedureFile.tsv"
 
-            # SCP will not overwrite or delete an existing file so we need to delete the server file first.
-            ssh_client = SSHClient()
-            ssh_client.load_system_host_keys()
-            ssh_client.set_missing_host_key_policy(AutoAddPolicy())
-            ssh_client.connect(hostname=host_ip, username='root', key_filename='C:{0}Users{0}robotron{0}ot2_ssh_key'.format(os.sep))
+        # SCP will not overwrite or delete an existing file so we need to delete the server file first.
+        ssh_client = SSHClient()
+        ssh_client.load_system_host_keys()
+        ssh_client.set_missing_host_key_policy(AutoAddPolicy())
+        try:
+            ssh_client.connect(hostname=host_ip, username='root',
+                               key_filename='C:{0}Users{0}robotron{0}ot2_ssh_key'.format(os.sep))
+        except OSError:
+            self.error_report("Unable to establish connection to robot for TSV file transfer.")
+            return
 
-            # Check if file exists
-            # ToDo: The remote exec command is not working.  Need to fix that!!!!!!!
-            file_delete = False
-            stdin, stdout, stderr = ssh_client.exec_command('stat {}'.format(server_path))
-            # stdin, stdout, stderr = ssh_client.exec_command('ls '.format(server_path))
+        # Check if file exists
+        # ToDo: The remote commands not working.  Need to fix that!!!!!!!
+        file_delete = False
+        stdin, stdout, stderr = ssh_client.exec_command('stat {}'.format(server_path))
+        # stdin, stdout, stderr = ssh_client.exec_command('ls '.format(server_path))
 
-            if len(stdout.readlines()) > 0:
-                file_delete = True
-                stdin, stdout, stderr = ssh_client.exec_command('rm {}'.format(server_path))
+        if len(stdout.readlines()) > 0:
+            file_delete = True
+            stdin, stdout, stderr = ssh_client.exec_command('rm {}'.format(server_path))
 
-            m = ""
-            if len(stderr.readlines()) > 0 and file_delete:
-                for line in stdout.readlines():
-                    m += m + line
+        m = ""
+        if len(stderr.readlines()) > 0 and file_delete:
+            for line in stdout.readlines():
+                m += m + line
 
-                self.error_report(m)
-                stderr.close()
-            elif len(stdout.readlines()) > 0 and file_delete:
-                for line in stdout.readlines():
-                    m += m + line
-                self.success_report(m, "Deletion")
+            self.error_report(m)
+            stderr.close()
+        elif len(stdout.readlines()) > 0 and file_delete:
+            for line in stdout.readlines():
+                m += m + line
+            self.success_report(m, "Deletion")
 
-            # stdout.close()
-            # stderr.close()
-            # stdin.close()
+        # Now transfer the new file to the robot.
+        scp = SCPClient(ssh_client.get_transport())
+        scp.put(files=self.path_to_tsv, remote_path=server_path, preserve_times=True)
+        scp.close()
+        ssh_client.close()
 
-            # Now transfer the new file to the robot.
-            scp = SCPClient(ssh_client.get_transport())
-            scp.put(files=self.path_to_file, remote_path=server_path, preserve_times=True)
-            scp.close()
-            ssh_client.close()
-
+        upload_success = True
+        if len(stdout.readlines()) > 0:
             upload_success = True
-            if len(stdout.readlines()) > 0:
-                upload_success = True
 
-            if not upload_success:
-                self.error_report("File Transfer Failed")
-            elif upload_success:
-                self.success_report("File Transferred Successfully", "File Transfer")
-
-
-
-            """
-            cmd = "scp -i ot2_ssh_key {} root@{}:{}".format(path_to_file, host_ip, server_path)
-            proc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-            if proc.stderr:
-                self.error_report(proc.stderr)
-                # dlg = QtWidgets.QMessageBox(self)
-                # dlg.setIcon(QtWidgets.QMessageBox.Critical)
-                # dlg.setWindowTitle("Well, that didn't go so well.")
-                # dlg.setText(proc.stderr.decode())
-                # dlg.exec_()
-            elif proc.stdout:
-                self.success_report(proc.stdout, "Transfer Succeeded")
-
-            """
+        if not upload_success:
+            self.error_report("File Transfer Failed")
+        elif upload_success:
+            self.success_report("File Transferred Successfully", "File Transfer")
 
     def simulate_run(self):
         # Todo: run simulation needs work
         if not self.selected_program:
             self.warning_report("Please Select Program for Simulation from dropdown list first.")
-        # p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
-        m = "Now you did it.\n  Life is but a simulation and you are trying to simulate a simulation.\n  " \
-            "This will cause reality to crash in on itself."
+        # Redirect stdout and stderr here so they can be displayed in the GUI
+        f = io.StringIO()
+        with redirect_stdout(f):
+            # Initialize template error checking
+            template_error_check = TemplateErrorChecking(self.path_to_tsv)
+            slot_error = template_error_check.slot_error_check()
 
-        self.run_simulation_output.insertPlainText(m)
+            if slot_error:
+                self.error_report("There is an error in the TSV Slot Definitions")
+                return
+
+            if self.selected_program == "Generic PCR":
+                error_msg = template_error_check.generic_pcr()
+
+                if error_msg:
+                    self.error_report(error_msg)
+                    return
+
+        self.run_simulation_output.insertPlainText('{}'.format(f.getvalue()))
+        self.path_to_program = "C:{0}Opentrons_Programs{0}Generic_PCR_v0.2.0.py".format(os.sep)
+        self.run_simulation_output.insertPlainText('Begin Program Simulation.\n\tErrors stop program and are reported '
+                                                   'in command window not GUI\n'.format(f.getvalue()))
+        try:
+            self.simulate_program()
+        except:
+            self.error_report("Simulation of {} Failed".format(os.path.basename(self.path_to_program)))
+            return
+
+        self.run_simulation_output.insertPlainText("\n")
+        self.success_report("Simulations were successful.", "Simulation Module")
+        os.remove(self.temp_tsv_path)
+        self.transfer_tsv_file()
+
+    def simulate_program(self):
+        # Select program file if not located where we think it is.
+        if not os.path.isfile(self.path_to_program):
+            self.path_to_program, _ = \
+                QtWidgets.QFileDialog.getOpenFileName(self, self.tr("Select Program File"),
+                                                      self.tr("C:{0}Users{0}robotron{0}Documents{0}".format(os.sep)))
+        protocol_file = open(self.path_to_program)
+
+        labware_location = "{}/custom_labware".format(os.path.dirname(self.path_to_program), os.sep)
+        run_log, __bundle__ = simulate(protocol_file, custom_labware_paths=[labware_location], propagate_logs=True)
+        self.run_simulation_output.insertPlainText(format_runlog(run_log))
+
+        protocol_file.close()
 
     def error_report(self, message):
         QtWidgets.QMessageBox.critical(self, "Well, that didn't go so well.", message)
@@ -151,12 +180,22 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         QtWidgets.QMessageBox.information(self, source, message)
 
 
-def center_window(centralWidget):
+def center_window(central_widget):
     screen_geometry = QtGui.QGuiApplication.primaryScreen().availableGeometry()
-    centralWidget.setWindowTitle("Opentrons Python Interface")
-    centralWidget.resize(screen_geometry.width()*0.52, screen_geometry.height()*0.85)
-    centralWidget.setGeometry(QtWidgets.QStyle.alignedRect(QtCore.Qt.LeftToRight, QtCore.Qt.AlignCenter,
-                                                           centralWidget.size(), screen_geometry, ), )
+    central_widget.setWindowTitle("Opentrons Python Interface")
+    w_scale = 0.52
+    h_scale = 0.8
+    if screen_geometry.width() > 2000:
+        w_scale = 0.27
+    elif screen_geometry.width() < 1500:
+        w_scale = 0.79
+    if screen_geometry.height() > 1200:
+        h_scale = 0.45
+    print(screen_geometry.width(), screen_geometry.height())
+    central_widget.resize(screen_geometry.width()*w_scale, screen_geometry.height()*h_scale)
+    central_widget.setGeometry(QtWidgets.QStyle.alignedRect(QtCore.Qt.LeftToRight, QtCore.Qt.AlignCenter,
+                                                            central_widget.size(), screen_geometry, ), )
+
 
 if __name__ == "__main__":
     app = QApplication([])
---
+++ TemplateErrorChecking.py
@@ -0,0 +1,265 @@
+
+"""
+Dennis A Simpson
+University of North Carolina at Chapel Hill
+450 West Drive
+Chapel Hill, NC  27599
+
+@Copyright 2021
+"""
+
+import sys
+from distutils import log
+from collections import defaultdict
+from types import SimpleNamespace
+import csv
+
+
+class TemplateErrorChecking:
+    def __init__(self, input_file):
+        self.stdout = sys.stdout
+        self.sample_dictionary, self.args = self.parse_sample_file(input_file)
+        self.pipette_info_dict = None
+        self.slot_dict = None
+        self.pipette_information()
+        self.well_label_dict = self.well_labels()
+
+    def parse_sample_file(self, input_file):
+        """
+        Parse TSV file
+        :param input_file:
+        :return:
+        """
+        line_num = 0
+        options_dictionary = defaultdict(str)
+        sample_dictionary = defaultdict(list)
+        index_file = list(csv.reader(open(input_file), delimiter='\t'))
+
+        for line in index_file:
+            # Get the program linked to the template file.  Data should always be the first cell of line one.
+            if line_num == 0:
+                options_dictionary["Program"] = line[0]
+
+            line_num += 1
+            col_count = len(line)
+            tmp_line = []
+            sample_key = ""
+            if col_count > 0 and "#" not in line[0] and len(line[0].split("#")[0]) > 0:
+                # Skip any lines that are blank or comments.
+
+                for i in range(6):
+                    try:
+                        line[i] = line[i].split("#")[0]  # Strip out end of line comments and white space.
+                    except IndexError:
+                        print("There is a syntax error in the TSV file on line {}, column {} "
+                              .format(str(line_num), str(i)))
+
+                        raise SystemExit(1)
+
+                    if i == 0 and "--" in line[0]:
+                        key = line[0].strip('--')
+                        options_dictionary[key] = line[1]
+                    elif "--" not in line[0] and int(line[0]) < 12:
+                        sample_key = line[0], line[1]
+                        tmp_line.append(line[i])
+                if sample_key:
+                    sample_dictionary[sample_key] = tmp_line
+
+        return sample_dictionary, SimpleNamespace(**options_dictionary)
+
+    def slot_error_check(self):
+        slot_error = False
+        slot_list = ["Slot1", "Slot2", "Slot3", "Slot4", "Slot5", "Slot6", "Slot7", "Slot8", "Slot9", "Slot10",
+                     "Slot11"]
+        slot_dict = {}
+        print("Checking Labware Definitions in Slots")
+
+        for i in range(len(slot_list)):
+            labware = getattr(self.args, "{}".format(slot_list[i]))
+
+            if labware and labware not in self.labware_slot_definitions:
+                print("ERROR: Slot {} labware definition not correct.".format(slot_list[i]))
+                slot_error = True
+            slot_dict[str(i + 1)] = labware
+
+        if slot_error:
+            print("NOTICE: There are errors in the labware definitions.  Correct these and run again\n")
+        else:
+            print("\t\tLabware definitions in slots passed")
+
+        self.slot_dict = slot_dict
+
+        return slot_error
+
+    def generic_pcr(self):
+        """
+        Perform error checking on the template for the Generic PCR program.
+        :return:
+        """
+        pipette_error = False
+
+        log.info("Checking Pipette Definitions")
+        if self.args.LeftPipette:
+            pipette_error = self.pipette_definition_error_check(pipette_error, self.args.LeftPipette, "Left Pipette")
+        if self.args.RightPipette:
+            pipette_error = self.pipette_definition_error_check(pipette_error, self.args.RightPipette, "Right Pipette")
+        if pipette_error:
+            msg = "There is an error in the one of the pipette definitions"
+            print("ERROR: {}".format(msg))
+
+            return msg
+        else:
+            print("\tPipette definitions passed.")
+
+        print("Checking Pipette Tip Box Definitions")
+        pipette_tip_box_error = False
+        if self.args.LeftPipette:
+            pipette_tip_box_error, lft_err_msg = \
+                self.pipette_tipbox_error_check(self.args.LeftPipetteTipRackSlot.split(","), pipette_tip_box_error,
+                                                self.args.LeftPipette, "Left pipette labware")
+        if self.args.RightPipette:
+            pipette_tip_box_error, rt_err_msg = \
+                self.pipette_tipbox_error_check(self.args.RightPipetteTipRackSlot.split(","), pipette_tip_box_error,
+                                                self.args.RightPipette, "Right pipette labware")
+        if pipette_tip_box_error:
+            if pipette_error:
+                print("WARNING: Pipette tip box definition error likely cause by the error in the pipette Definitions")
+            else:
+                msg = "{}{}".format(lft_err_msg, rt_err_msg)
+                print("ERROR: {}".format(msg))
+                return msg
+        else:
+            print("\tPipette tip box definitions passed")
+
+        # Check the reagent slot and reagent wells
+        if not self.args.ReagentSlot:
+            msg = 'Reagent Slot definition missing.'
+            print("ERROR: {}".format(msg))
+            return msg
+
+        reagent_slot = self.args.ReagentSlot
+        reagent_labware = self.slot_dict[reagent_slot]
+        for pipette in self.pipette_info_dict:
+            if reagent_labware == self.pipette_info_dict[pipette]:
+                msg = "Reagent slot contains a pipette tip box"
+                print("ERROR: {}".format(msg))
+                return msg
+
+        water_control_slot = self.args.WaterControl.split(",")[0]
+        water_control_labware = self.slot_dict[water_control_slot]
+        water_control_well = self.args.WaterControl.split(",")[1]
+
+        water_control_labware_pass = False
+        reagent_labware_pass = False
+
+        for key in self.well_label_dict:
+            if key in water_control_labware:
+                water_control_labware_pass = True
+                label_list = self.well_label_dict[key]
+                if water_control_well not in label_list:
+                    msg = "The well defined for the Water Control Sample is not possible for {}"\
+                        .format(water_control_labware)
+                    print("ERROR: {}".format(msg))
+                    return msg
+
+            if key in reagent_labware:
+                reagent_labware_pass = True
+                label_list = self.well_label_dict[key]
+                if self.args.WaterWell not in label_list:
+                    msg = "The water well definition is not possible for {}".format(reagent_labware)
+                    print("ERROR: {}".format(msg))
+                    return msg
+
+                if self.args.PCR_MixWell not in label_list:
+                    msg = "The well defined for the PCR Mix is not possible for {}".format(reagent_labware)
+                    print("ERROR: {}".format(msg))
+                    return msg
+
+        if not water_control_labware_pass:
+            print("The Water Control labware is not correctly defined.")
+        if not reagent_labware_pass:
+            print("ERROR: The Reagent labware is not correctly defined.")
+
+        # Process Sample data;
+        source_test = []
+        dest_test = []
+        for sample_key in self.sample_dictionary:
+            sample_source_slot = self.sample_dictionary[sample_key][0]
+            sample_source_well = self.sample_dictionary[sample_key][1]
+            sample_dest_slot = self.sample_dictionary[sample_key][4]
+            # sample_dest_labware = self.labware_slot_definitions[int(sample_dest_slot)]
+            sample_dest_well = self.sample_dictionary[sample_key][5]
+            source_test.append("{}+{}".format(sample_source_slot, sample_source_well))
+
+            # If there are replicates a single sample can have more than one destination well.
+            for well in sample_dest_well:
+                dest_test.append("{}+{}".format(sample_dest_slot, well))
+        for source in source_test:
+            if source in dest_test:
+                msg = "More than one sample share the same source and/or destinations"
+                print("ERROR:  {}".format(msg))
+                return msg
+
+    @property
+    def labware_slot_definitions(self):
+        """
+        Labware that we have on-hand.
+        :return:
+        """
+        labware_list = [
+            "opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap", "stacked_96_well", "8_well_strip_tubes_200ul",
+            "opentrons_96_tiprack_10ul", "opentrons_96_tiprack_20ul", "opentrons_96_tiprack_300ul"]
+
+        return labware_list
+
+    def well_labels(self):
+        """
+        Create a dictionary of well labels for each loaded labware.
+        :return:
+        """
+
+        def well_list(row_count, column_count):
+            row_labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+                          "T", "U", "V", "W", "X", "Y", "Z"]
+            temp_list = []
+            for r in range(row_count):
+                for i in range(column_count):
+                    temp_list.append("{}{}".format(row_labels[r], i + 1))
+
+            return temp_list
+
+        well_labels_dict = defaultdict(list)
+        for labware in self.labware_slot_definitions:
+            w96 = well_list(8, 12)
+            w24 = well_list(4, 6)
+
+            if "24" in labware:
+                well_labels_dict[labware] = w24
+            elif "96" in labware:
+                well_labels_dict[labware] = w96
+            elif len(well_labels_dict) == 0:
+                msg = "Well label definitions failed.  Incorrect labware passed.  Template file is bad"
+                print("ERROR:  {}".format(msg))
+                return msg
+
+        return well_labels_dict
+
+    def pipette_information(self):
+        self.pipette_info_dict = {"p10_multi": "opentrons_96_tiprack_10ul", "p10_single": "opentrons_96_tiprack_10ul",
+                                  "p20_single_gen2": "opentrons_96_tiprack_20ul",
+                                  "p300_single_gen2": "opentrons_96_tiprack_300ul"}
+
+    def pipette_tipbox_error_check(self, pipette_tip_slots, error_state, pipette, msg):
+        pipette_labware = self.pipette_info_dict[pipette]
+        err_msg = ""
+        for slot in pipette_tip_slots:
+            if self.slot_dict[slot] != pipette_labware:
+                err_msg = "ERROR:  {} in slot {} is not correct.".format(msg, slot)
+                error_state = True
+        return error_state, err_msg
+
+    def pipette_definition_error_check(self, error_state, pipette, pipette_str):
+        if pipette not in self.pipette_info_dict:
+            error_state = True
+            print("ERROR:  {} definition not correct".format(pipette_str))
+        return error_state
